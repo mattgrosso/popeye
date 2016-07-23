f352fe561052c6ef88489c1915c79ee078194c9c
MJG <MJG@MacBook-Pro.local>
2016-07-22 20:54:34 -0400

    Got things started with geolocation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -41,7 +41,7 @@ module.exports = function(grunt) {
           sourceMap: true
         },
         js: {
-          src: ['src/**/*.js'],
+          src: ['src/js/popeye.module.js', 'src/**/*.js'],
           dest: 'build/js/main.js'
         }
       },
--- build/js/main.js
+++ build/js/main.js
@@ -1,2 +1,42 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('popeye', ['ui.router', 'ngStorage'])
+    .config(popeyeConfig);
+
+  popeyeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+  function popeyeConfig($stateProvider, $urlRouterProvider) {
+    console.log($stateProvider, $urlRouterProvider);
+  }
+})();
+
+(function() {
+  'use strict';
+
+  angular
+    .module('popeye')
+    .controller('MainController', MainController);
+
+  function MainController() {
+
+    this.geoLocate = function geoLocate() {
+      if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          console.log(position);
+          return {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+          };
+        });
+      } else {
+        return 'not available';
+      }
+    };
+
+    this.geoLocate();
+  }
+})();
 
 //# sourceMappingURL=main.js.map
